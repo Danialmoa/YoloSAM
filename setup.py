48c642e043d1e398c5b4c1ced74774d30cd173f9
Danial Moafi <danialmoafi@gmail.com>
2025-06-24 17:26:34 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yolosam",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchvision", 
+        "numpy",
+        "Pillow",
+        "albumentations",
+        "segment-anything @ git+https://github.com/facebookresearch/segment-anything.git",
+    ],
+    python_requires=">=3.8",
+)
